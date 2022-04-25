1511b54b76326309613e1e58ad017b9a10e61b41
401 <naime2008@hotmail.com>
2022-04-25 15:38:22 -0500

    NTG-001-SE CREA REPOSITORIO EJERCICIO RAMAS GIT

---
+++ Personaje.py
@@ -0,0 +1,14 @@
+class Personaje:
+    #CONSTRUCTOR
+    #METODO O FUNCION ESPECIAL ENCARGADO DE INICIALIZAR LOS ATRIBUTOS DE UNA CLASE
+    def __init__(self,nombre,tipo,casaEditorial):
+
+        #CREAMOS LOS ATRIBUTOS DE LA CLASE 
+        #ATRIBUTOS=DATOS=VARIABLE
+        self.nombre=nombre
+        self.tipo=tipo
+        self.casaeditorial=casaEditorial
+
+    #DECLARO LOS METODOS O FUNCIONES DE MI CLASE
+    def saludar(self):
+        print("Hola como estas...")
---
+++ main.py
@@ -0,0 +1,14 @@
+from Personaje import Personaje
+
+#CREAMOS UNA COPIA DE LA CLASE
+#UTILIZAMOS LA CLASE
+#CREAMOS UNA INSTANCIA DE LA CLASE
+#CREAMOS UN OBJETO DE LA CLASE
+personaje= Personaje("BATMAN","HEROE","DC")
+
+#acceder a los atributos de la clase por medio del objeto
+
+print(personaje.nombre)
+
+#acceder a los metodos de la clase por medio del objeto
+personaje.saludar()
