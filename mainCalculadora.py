ac3c56eee18e231f9258719b9d3c1f7adc1e4506
401 <401@academicos.cesde.edu.co>
2022-04-25 15:47:43 -0500

    HU01 se creo archivo mainCalculadora

---
+++ mainCalculadora.py
@@ -0,0 +1,8 @@
+from Calculadora import Calculadora
+
+calculadora= Calculadora(5,10)
+
+print(calculadora.sumar())
+print(calculadora.restar())
+print(calculadora.mutiplicar())
+print(calculadora.dividir())
